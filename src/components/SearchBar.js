84bd1080941336a3122c5dbfff46fc0d3b61db0b
Indira Diana Puspasari <indiradiana239@gmail.com>
2023-06-07 16:52:25 +0700

    little change

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,22 @@
+import { useState } from "react";
+
 function SearchBar({ onSubmit }) {
-  const handleClick = () => {};
+  const [term, setTerm] = useState("");
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log("");
+  };
+
+  const handleChange = (event) => {
+    setTerm(event.target.value);
+  };
 
   return (
     <div>
-      <input />
-      <button onClick={handleClick}>Click</button>
+      <form on Submit={handleFormSubmit}>
+        <input value={term} onChange={handleChange} />
+      </form>
     </div>
   );
 }
